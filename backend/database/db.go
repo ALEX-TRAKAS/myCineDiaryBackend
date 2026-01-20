c72aaf073e2f2ab5a88790e2948678e429569889
Alex-Trakas <57215703+ALEX-TRAKAS@users.noreply.github.com>
2026-01-20 22:40:10 +0200

    Refactor(db)Change pgx to pgxpool

--- backend/database/db.go
+++ backend/database/db.go
@@ -5,25 +5,38 @@ import (
 	"log"
 	"mycinediarybackend/config"
 	"os"
+	"time"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-var DB *pgx.Conn
+var DB *pgxpool.Pool
 
 func Connect() {
 	config.Load()
-	conn, err := pgx.Connect(context.Background(), config.GetEnv("DATABASE_URL", os.Getenv("DATABASE_URL")))
+
+	dsn := config.GetEnv("DATABASE_URL", os.Getenv("DATABASE_URL"))
+
+	cfg, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
-		log.Fatalf("Failed to connect to the database: %v", err)
+		log.Fatalf("Failed to parse DB config: %v", err)
+	}
+
+	cfg.MaxConns = 10
+	cfg.MinConns = 2
+	cfg.MaxConnLifetime = time.Hour
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), cfg)
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %v", err)
 	}
 
 	var version string
-	if err := conn.QueryRow(context.Background(), "SELECT version()").Scan(&version); err != nil {
-		log.Fatalf("Query failed: %v", err)
+	if err := pool.QueryRow(context.Background(), "SELECT version()").Scan(&version); err != nil {
+		log.Fatalf("DB test query failed: %v", err)
 	}
 
 	log.Println("Connected to:", version)
 
-	DB = conn
+	DB = pool
 }
