38d3b81894bae9a71be179af0b5add263a17db2c
Alex-Trakas <57215703+ALEX-TRAKAS@users.noreply.github.com>
2026-02-27 17:45:41 +0200

    Refactor(UserMedia model)ADD avgRating,ReviewsCount for rating calculation based on the reviews

--- backend/models/user_media.go
+++ backend/models/user_media.go
@@ -10,17 +10,20 @@ const (
 )
 
 type Media struct {
-	ID           uint64     `json:"id"`
-	TMDBID       int        `json:"tmdb_id"`
-	MediaType    MediaType  `json:"media_type"`
-	Title        string     `json:"title"`
-	Overview     string     `json:"overview"`
-	PosterPath   string     `json:"poster_path"`
-	BackdropPath string     `json:"backdrop_path"`
-	ReleaseDate  *time.Time `json:"release_date,omitempty"`
-	Popularity   float64    `json:"popularity"`
-	VoteAverage  float64    `json:"vote_average"`
-	CachedAt     time.Time  `json:"cached_at"`
+	ID            uint64     `json:"id"`
+	TMDBID        int        `json:"tmdb_id"`
+	MediaType     MediaType  `json:"media_type"`
+	Title         string     `json:"title"`
+	Overview      string     `json:"overview"`
+	PosterPath    string     `json:"poster_path"`
+	BackdropPath  string     `json:"backdrop_path"`
+	ReleaseDate   *time.Time `json:"release_date,omitempty"`
+	Popularity    float64    `json:"popularity"`
+	VoteAverage   float64    `json:"vote_average"`
+	AverageRating float64    `json:"average_rating" gorm:"not null;default:0"`
+	ReviewsCount  int        `json:"reviews_count" gorm:"not null;default:0"`
+
+	CachedAt time.Time `json:"cached_at"`
 }
 
 type MediaStatus string
